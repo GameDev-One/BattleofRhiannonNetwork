427d09ef273b3e3bbcc04a1fb1789a462c10b833
Devone Reynolds <gamedevone1@gmail.com>
2021-06-07 09:58:57 -0700

    Created Succulent Shot Chip

---
+++ src/player/battle/chip/projectile/projectile.gd
@@ -0,0 +1,39 @@
+class_name Projectile
+extends RigidBody
+
+export var speed: float = 10.0
+
+export(NodePath) var collision_shape
+export(NodePath) var anticipation_path
+export(NodePath) var body_path
+export(NodePath) var impact_path
+
+onready var _collision_shape: CollisionShape = get_node(collision_shape)
+onready var _anticipation: CPUParticles = get_node(anticipation_path)
+onready var _body: CPUParticles = get_node(body_path)
+onready var _impact: CPUParticles = get_node(impact_path)
+
+var damage: int = 0
+var is_shot: bool = false
+
+
+func _ready():
+	set_as_toplevel(true)
+	_emit_anticipation()
+
+
+func _physics_process(delta):
+	if is_shot:
+		apply_impulse(transform.basis.z, -transform.basis.z * speed / 100)
+
+
+func _emit_anticipation() -> void:
+	_anticipation.emitting = true
+	for p in _anticipation.get_children():
+		p.emitting = true
+
+
+func _emit_impact() -> void:
+	_impact.emitting = true
+	for p in _impact.get_children():
+		p.emitting = true
---
+++ src/player/battle/chip/projectile/succulent_burst/succulent_burst.gd
@@ -0,0 +1,36 @@
+extends Projectile
+
+func _ready():
+	set_as_toplevel(true)
+	set_physics_process(false)
+	_emit_anticipation()
+	
+
+func _physics_process(delta):
+	if is_shot:
+		apply_impulse(transform.basis.z, -transform.basis.z * speed / 100)
+		is_shot = false
+		_body.emitting = true
+#		_trail.emitting = true
+
+
+func _emit_anticipation() -> void:
+	_anticipation.emitting = true
+	yield(get_tree().create_timer(_anticipation.lifetime), "timeout")
+	set_physics_process(true)
+
+
+func _on_SucculentBurst_body_entered(body):
+	if body.is_in_group("Enemy"):
+		body.health -= damage
+		body.dmg_ind_particles.value = damage
+		_collision_shape.disabled = true
+		_body.hide()
+		_impact.global_transform.origin = body.global_transform.origin + Vector3(0,1,0)
+		_emit_impact()
+	else:
+		queue_free()
+
+
+func _on_Lifetime_timeout():
+	queue_free()
